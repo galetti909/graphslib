5a6bb43a3cda1e761ec635c6d538c2c8493e893c
lfcavalini <131682085+LuizCavalini@users.noreply.github.com>
2025-09-15 13:27:27 -0300

    fix: adjust indexion to base 0

--- adjacency_matrix.py
+++ adjacency_matrix.py
@@ -3,15 +3,19 @@ from generic_structure import GraphStructure
 class AdjacencyMatrix(GraphStructure):
     def __init__(self, file_path: str) -> None:
         with open(file_path, 'r') as f:
+            node_count = int(f.readline().strip())
             self.adjacency_matrix = [
-                [0] * (node_count := int(f.readline().strip())) 
+                [0] * node_count 
                 for _ in range(node_count)
             ]
 
             for line in f.readlines():
                 nodes = line.strip().split()
-                if len(nodes) == 2: 
-                    self.add_edge(int(nodes[0]), int(nodes[1]))
+                if len(nodes) == 2:
+                    # fix: adjust indexing to 0
+                    node_1 = int(nodes[0]) - 1
+                    node_2 = int(nodes[1]) - 1
+                    self.add_edge(node_1, node_2)
 
     def add_edge(self, node_1: int, node_2: int):
         self.validate_node_index(node_1, node_2)
