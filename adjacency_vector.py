35aa543f3396c101e5b669799517e024e9bf43c7
lfcavalini <131682085+LuizCavalini@users.noreply.github.com>
2025-09-15 13:33:37 -0300

    fix: adjust indexing to base 0 and remove the possibility of the duplicates edges

--- adjacency_vector.py
+++ adjacency_vector.py
@@ -3,19 +3,27 @@ from generic_structure import GraphStructure
 class AdjacencyVector(GraphStructure):
     def __init__(self, file_path: str) -> None:
         with open(file_path, 'r') as f:
-            self.adjacency_vector = [[]] * int(f.readline().strip())
+            node_count = int(f.readline().strip())
+            # Correct initialization to avoid all lists being the same reference
+            self.adjacency_vector = [[] for _ in range(node_count)]
 
             for line in f.readlines():
                 nodes = line.strip().split()
-                if len(nodes) == 2: 
-                    self.add_edge(int(nodes[0]), int(nodes[1]))
+                if len(nodes) == 2:
+                    # fix: adjust indexion to base 0
+                    node_1 = int(nodes[0]) - 1
+                    node_2 = int(nodes[1]) - 1
+                    self.add_edge(node_1, node_2)
 
     def add_edge(self, node_1: int, node_2: int):
         self.validate_node_index(node_1, node_2)
         if node_1 == node_2:
             raise ValueError('Self-edges are not allowed.')
-        self.adjacency_vector[node_1] += [node_2]
-        self.adjacency_vector[node_2] += [node_1]
+        # ensure there aren't duplicates edges
+        if node_2 not in self.adjacency_vector[node_1]:
+            self.adjacency_vector[node_1].append(node_2)
+        if node_1 not in self.adjacency_vector[node_2]:
+            self.adjacency_vector[node_2].append(node_1)
 
     def get_node_count(self) -> int:
         return len(self.adjacency_vector)
