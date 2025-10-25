0ab012c8eae6da899ea9fdc8a86f8d7793e56cec
galetti909 <joaogaletti@gmail.com>
2025-10-25 20:19:29 -0300

    fix: has negative weight property

--- lib/adjacency_matrix.py
+++ lib/adjacency_matrix.py
@@ -3,6 +3,7 @@ from .generic_structure import GraphStructure
 class AdjacencyMatrix(GraphStructure):
     def __init__(self, file_path: str) -> None:
         try:
+            self._has_negative_weight = False
             with open(file_path, 'r') as f:
                 node_count = int(f.readline().strip())
                 self.adjacency_matrix = [
@@ -21,10 +22,14 @@ class AdjacencyMatrix(GraphStructure):
     def add_edge(self, node_1: int, node_2: int, weight: float = 1.0) -> None:
         self.validate_node_index(node_1, node_2)
         if weight < 0:
-            self.has_negative_weight = True
+            self._has_negative_weight = True
         self.adjacency_matrix[node_1][node_2] = weight
         self.adjacency_matrix[node_2][node_1] = weight
 
+    @property
+    def has_negative_weight(self) -> bool:
+        return self._has_negative_weight
+    
     def get_node_count(self) -> int:
         return len(self.adjacency_matrix) - 1
 
--- lib/adjacency_vector.py
+++ lib/adjacency_vector.py
@@ -3,6 +3,7 @@ from .generic_structure import GraphStructure
 class AdjacencyVector(GraphStructure):
     def __init__(self, file_path: str) -> None:
         with open(file_path, 'r') as f:
+            self._has_negative_weight = False
             node_count = int(f.readline().strip())
             self.adjacency_vector = [[] for _ in range(node_count + 1)]
 
@@ -14,10 +15,14 @@ class AdjacencyVector(GraphStructure):
     def add_edge(self, node_1: int, node_2: int, weight: float = 1.0) -> None:
         self.validate_node_index(node_1, node_2)
         if weight < 0:
-            self.has_negative_weight = True
+            self._has_negative_weight = True
         if node_2 not in self.adjacency_vector[node_1]:
             self.adjacency_vector[node_1].append((node_2, weight))
             self.adjacency_vector[node_2].append((node_1, weight))
+
+    @property
+    def has_negative_weight(self) -> bool:
+        return self._has_negative_weight
 
     def get_node_count(self) -> int:
         return len(self.adjacency_vector) - 1
